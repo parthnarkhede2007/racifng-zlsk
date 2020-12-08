c0abfe201499069567edb8d2dc9c2607642c147f
parthnarkhede2007 <73740062+parthnarkhede2007@users.noreply.github.com>
2020-12-08 19:17:28 +0530

    Add files via upload

---
+++ synchronousBallMovement-master/sketch.js
@@ -0,0 +1,18 @@
+var database,playerCount,form, player,game;
+var gameState = 0;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+game = new Game();
+game.getState();
+game.start();
+}
+
+function draw(){
+ 
+    
+  
+}
